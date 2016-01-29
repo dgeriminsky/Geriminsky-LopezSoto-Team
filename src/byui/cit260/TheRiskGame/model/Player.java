7f89a835d5b3c266eac7c62ba3d6d85822c407d5
David Geriminsky <dgeriminsky@gmail.com>
2016-01-28 22:21:22 -0700

    01/28/2016 - Initial Commit to Repository

---
+++ src/byui/cit260/TheRiskGame/model/Player.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TheRiskGame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author David
+ */
+public class Player implements Serializable{
+    
+    //Class Instance Variables
+    private String name;
+    private String description;
+
+    public Player() {
+    } 
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", description=" + description + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+            
+    
+}
---
+++ src/theriskgame/TheRiskGame.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theriskgame;
+
+import byui.cit260.TheRiskGame.model.Player;
+
+
+/**
+ *
+ * @author David
+ */
+public class TheRiskGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Player playerOne = new Player();
+        
+        playerOne.setName("Lehi Lopez");
+        playerOne.setDescription("One Tough Dude");
+                
+        String playerInfo = playerOne.toString();    
+        System.out.println(playerInfo);
+                
+                
+    }
+    
+    
+    
+    
+}

bbcbc738976a6619e9f0fdc940be9490d3dbdf96
LZ <LZ@LZ-HP>
2016-02-20 00:14:26 -0600

    2/19/2016 Create MainMenuVuew class

---
+++ src/byui/cit260/TheRiskGame/view/MainMenuView.java
@@ -0,0 +1,18 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TheRiskGame.view;
+
+/**
+ *
+ * @author LZ
+ */
+public class MainMenuView {
+
+    void displayMainMenuView() {
+        System.out.println("/n*** displayMainMenuView() function called ***");
+    }
+    
+}
